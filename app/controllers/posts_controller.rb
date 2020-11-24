7603f7f5fc7ab9ab00898398cda0e2ad97ab057d
Tomasz Szczęśniak-Szlagowski <spect88@gmail.com>
2020-11-24 23:23:50 +0900

    Uncomment code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-	 # before_action :find_post, only: [:show, :update, :edit, :destroy]
+	 before_action :find_post, only: [:show, :update, :edit, :destroy]
 
 	def index
 		@posts = Post.all.order("created_at DESC")
@@ -49,8 +49,8 @@ class PostsController < ApplicationController
 		params.require(:post).permit(:title, :content)
 	end
 
-	# def find_post
-	# 	@post = Post.find(params[:id])
-	# end
+	def find_post
+		@post = Post.find(params[:id])
+	end
 
 end
