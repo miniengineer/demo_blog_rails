97790b18b7d5955bc133fa5742cf0df127032de1
Tomasz Szczęśniak-Szlagowski <spect88@gmail.com>
2020-11-24 23:30:22 +0900

    Auto-correct style with Rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts do
-  	resources :comments
+    resources :comments
   end
 
   root 'posts#index'
